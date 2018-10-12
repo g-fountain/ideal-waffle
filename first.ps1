0526f71e83f39b40995a24198468fb4b390f9425
G <g.fountain@sky.comgit config --global user.name Ggit config --getallgit config --global user.email g.fountain@sky.com>
2018-10-12 13:36:21 +0100

    c

--- first.ps1
+++ first.ps1
@@ -1,2 +1,10 @@
-#First page
-if
+#First 
+Import-Module VMware.VimAutomation.Core
+$xml_data_path = "D:\Projects\ideal-waffle\data.xml" 
+$xml_data = [xml](Get-Content $xml_data_path)
+
+
+write-host $xml_data.config.conn.vcenter_user -password $xml_data.config.conn.vcenter_password
+connect-viserver $xml_data.config.conn.vcenter_FQDN -user $xml_data.config.conn.vcenter_user -password $xml_data.config.conn.vcenter_password -WarningAction SilentlyContinue
+
+Disconnect-VIServer $xml_data.config.conn.vcenter_FQDN -Confirm:$false
