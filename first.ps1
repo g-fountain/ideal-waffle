8c9de054abfe386b41e19b1f3bd1796c4e9504de
G <g.fountain@sky.comgit config --global user.name Ggit config --getallgit config --global user.email g.fountain@sky.com>
2018-10-12 14:59:22 +0100

    2

--- first.ps1
+++ first.ps1
@@ -2,9 +2,8 @@
 Import-Module VMware.VimAutomation.Core
 $xml_data_path = "D:\Projects\ideal-waffle\data.xml" 
 $xml_data = [xml](Get-Content $xml_data_path)
-
-
-write-host $xml_data.config.conn.vcenter_user -password $xml_data.config.conn.vcenter_password
+$vm = 'Linux Mint'
+$updateinfo = Get-Random
 connect-viserver $xml_data.config.conn.vcenter_FQDN -user $xml_data.config.conn.vcenter_user -password $xml_data.config.conn.vcenter_password -WarningAction SilentlyContinue
-
+Get-VM -Name 'Linux Mint' | set-vm -Description $vm -Confirm:$false -RunAsync
 Disconnect-VIServer $xml_data.config.conn.vcenter_FQDN -Confirm:$false
